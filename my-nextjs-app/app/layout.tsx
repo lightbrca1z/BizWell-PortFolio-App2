780e55bdae2cc9163535fe8ede1576678437324e
lightbrca1z <quwieorpt@gmail.com>
2025-07-15 11:28:40 +0900

    Next.jsのダイナミックルーティングについて。

--- my-nextjs-app/app/layout.tsx
+++ my-nextjs-app/app/layout.tsx
@@ -14,9 +14,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ja">
-      <body
-     
-      >
+      <body className="p-4">
         {children}
       </body>
     </html>
